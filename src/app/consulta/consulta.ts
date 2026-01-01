ec8ce5c843ef15a0d13ad824fe261e50548f630d
DiorgenesSouza <diorgeneslimadesouza833@gmail.com>
2026-01-01 11:43:47 -0300

    Implementação das colunas do resultado da pesquisa

--- src/app/consulta/consulta.ts
+++ src/app/consulta/consulta.ts
@@ -1,28 +1,45 @@
-import { Component } from '@angular/core';
-import { MatInputModule } from '@angular/material/input'
-import { MatCardModule } from '@angular/material/card'
-import { FlexLayoutModule } from '@angular/flex-layout'
-import { MatIconModule } from '@angular/material/icon'
-import { FormsModule } from '@angular/forms'
-import { MatTableModule } from '@angular/material/table'
-import { MatAnchor } from "@angular/material/button";
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+
+import { ClienteService } from '../cliente-service';
+import { Cliente } from '../cadastro/cliente';
 
 @Component({
   selector: 'app-consulta',
+  standalone: true,
   imports: [
-    MatInputModule,
-    MatCardModule,
-    MatIconModule,
-    MatTableModule,
-    MatButtonModule,
-    FlexLayoutModule,
+    CommonModule,
     FormsModule,
-    MatAnchor
-],
+    FlexLayoutModule,
+
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule
+  ],
   templateUrl: './consulta.html',
   styleUrl: './consulta.scss',
 })
-export class Consulta {
+export class Consulta implements OnInit {
 
+  listaClientes: Cliente[] = [];
+  colunasTable: string[] = ['id', 'nome', 'cpf', 'dataNascimento', 'email'];
+
+  constructor(private service: ClienteService) {}
+
+  ngOnInit(): void {
+    this.listaClientes = this.service.pesquisarClientes('');
+  }
 }
